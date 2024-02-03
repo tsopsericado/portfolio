f685a7ead8a8f71ee8f279b771b1ae4b8715c4f4
ricardo <ricardotsopse@gmail.com>
2024-02-03 18:54:33 +0100

    some changes made

---
+++ src/app/api/education/add/route.js
@@ -0,0 +1,33 @@
+import connectToDB from "@/app/database";
+import Home from "@/models/Home";
+
+import { NextResponse } from "next/server";
+
+export const dynamic = "force-dynamic";
+
+export async function POST(req) {
+  try {
+    await connectToDB();
+    const extractData = await req.json();
+    const saveData = await Home.create(extractData);
+
+    if (saveData) {
+      return NextResponse.json({
+        success: true,
+        message: "Data saved succesfully",
+      });
+    } else {
+      return NextResponse.json({
+        success: false,
+        message: "something went wrong! Please try again",
+      });
+    }
+  } catch (e) {
+    console.log(e);
+
+    return NextResponse.json({
+      success: false,
+      message: "something went wrong! Please try again",
+    });
+  }
+}
